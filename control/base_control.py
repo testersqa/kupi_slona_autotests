333f67761b627567119ab005f735c845ea2fa3b6
Nastya <gaa@top.corp>
2026-01-28 20:33:43 +0500

    Объеденила control и controls

--- control/base_control.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import expect
-
-class BaseControl:
-    def __init__(self, page):
-        self.page = page
-
-    def is_visible(self):
-        return expect(self.page).to_be_visible()
