a635d6a952e6ad165eb3a8a82ce7d28cc34e33cf
MNOGOIGRAAL <artur-ismailov-01@mail.ru>
2026-01-28 16:42:36 +0500

    Правки в conftest

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
 from playwright.sync_api import sync_playwright
+import pytest
 
 
 @pytest.fixture(scope="function", autouse=False)
-def driver():
+def page():
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch(headless=False)
     page = browser.new_page()
     yield page
     browser.close()
     playwright.stop()  
-    playwright.stop()
