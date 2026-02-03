adaa5f6427dc8ed69b1a8e065b5dc9511ffe46ad
Gul4ik <Gul4ik_793@mail.ru>
2026-02-03 22:06:24 +0500

    добавила проверки к автотесту 6 и к тест кейсу №6

--- tests/test_product_bue.py
+++ tests/test_product_bue.py
@@ -10,11 +10,22 @@ from pages.product_buy_page import ProductBuePage
 def test_buy_product_with_zero_quantity(page):
     product = ProductBuePage(page, "https://kupislona-store.ru/katalog/all")
 
-    with allure.step("Открытие страницы https://kupislona-store.ru/katalog/all"):
+    with allure.step("Шаг 1: Открыть сайт https://kupislona-store.ru/katalog/all"):
         product.open()
+        expect(page).to_have_title("| Купи слона - Магазины классных вещиц")
 
-    with allure.step("Установить количество товара на 0"):
-        product.set_quantity("0")
+    with allure.step("Шаг 2: Под товаром «Блокнот «Охотник»» очистить поле ввода количества товаров"):
+        product.set_quantity()
+        locator = page.locator("#edit-qty--2")
+        expect(locator).to_have_value("")
 
-    with allure.step("Нажать кнопку 'Купить'"):
+    with allure.step("Шаг 3: ввести значение «0» в поле количество товаров"):
+        product.set_quantity0("0")
+        locator = page.locator("#edit-qty--2")
+        expect(locator).to_have_value("0")
+
+    with allure.step("Шаг 4: Нажать кнопку «купить»"):
         product.click_buy_button()
+        locator = page.locator(".messages.error")
+        expect(locator).to_be_visible(timeout=5000)
+        expect(locator).to_contain_text("Сообщение об ошибке")
