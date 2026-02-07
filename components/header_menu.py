e8151dc5e5529dea5d91b8a6d100d3b6847f6612
An <12341@>
2026-02-07 21:37:29 +0500

    Изменение компонента HeaderMenu

--- components/header_menu.py
+++ components/header_menu.py
@@ -1,3 +1,4 @@
+from playwright.sync_api import Locator
 from components.base_component import BaseComponent
 
 
@@ -5,5 +6,8 @@ class HeaderMenu(BaseComponent):
     def __init__(self, page):
         super().__init__(page, page.locator("#header_menu"))
 
-    def click_name_catalog(self, name: str):
-        self.page.locator(f"a:has-text('{name}')").click()
+    def get_menu_link(self, title: str,) -> Locator:
+        return self.page.locator(f"a:has-text('{title}')")
+
+    def click_name_catalog(self, title: str):
+        self.get_menu_link(title).click()
